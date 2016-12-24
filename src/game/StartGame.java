9b71e185f22dbf2a650b93bd0fa7be0057b446f8
zorsin <zorsinthedeath@gmail.com>
2016-12-24 17:09:46 +0100

    Kommentare hinzugefuegt

--- src/game/StartGame.java
+++ src/game/StartGame.java
@@ -15,12 +15,14 @@ public class StartGame {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		/**
+		 * diese Klasse dient nur dem starten des Spiels
+		 */
+		BasicUI ui = new BasicUI();//iniziallisiert das Spiel
 		
-		BasicUI ui = new BasicUI();
-		
-		boolean run = ui.startGame();
+		boolean run = ui.startGame();//startet die erste Runde
 		while(run){
-			run = ui.startGame();
+			run = ui.startGame();//solange das Spiel nicht beendet wurde, wird das Spiel (Menu) erneut angezeigt
 		}
 		ui.prln("Spiel wurde beendet!");
 	}
