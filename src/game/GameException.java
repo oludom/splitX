b8bda5741b988b9e75f044f0b3db064381e7a8c7
oludom <mhlauffen@web.de>
2016-12-28 12:02:02 +0100

    Issue, Feature UI: Spiel beenden hinzugefügt. der Spieler hat die Möglichkeit durch Eingabe von z.B. "exit", "quit" das Spiel zu beenden und zum Menü zurück zukehren.

--- src/game/GameException.java
+++ src/game/GameException.java
@@ -61,15 +61,28 @@ public class GameException extends Exception{
 			return errorMsg;
 		}
 	}
-	
+
 	static public class BoardFullException extends GameException{
 		private static final long serialVersionUID = 1L;
 		String errorMsg = "";
-		
+
 		public BoardFullException(){
 			errorMsg = "Alle Felder auf dem Spielfeld wurden belegt. Das Spiel endet unentschieden.";
 		}
-		
+
+		public String toString(){
+			return errorMsg;
+		}
+	}
+
+	static public class StopGameException extends GameException{
+		private static final long serialVersionUID = 1L;
+		String errorMsg = "";
+
+		public StopGameException(){
+			errorMsg = "Du hast das Spiel beendet!";
+		}
+
 		public String toString(){
 			return errorMsg;
 		}
